3214a4a46de6e080b4e8df0472fbea195d3883ab
Igor Gapanovich <igapanovich@proton.me>
2026-01-24 19:25:17 +0100

    Added command line option descriptions

--- src/cli_arguments.rs
+++ src/cli_arguments.rs
@@ -15,6 +15,7 @@ pub enum Commands {
         #[arg(short = 'f', long = "file", env = "BIVER_PATH")]
         versioned_file_path: PathBuf,
 
+        /// Show all versions (by default, limited to 20 most recent)
         #[arg(short = 'a', long = "all")]
         all: bool,
     },
@@ -25,9 +26,11 @@ pub enum Commands {
         #[arg(short = 'f', long = "file", env = "BIVER_PATH")]
         versioned_file_path: PathBuf,
 
+        /// New branch to create
         #[arg(short = 'b', long = "branch")]
         branch: Option<String>,
 
+        /// Description of the new version
         #[arg(value_name = "DESCRIPTION")]
         description: Option<String>,
     },
