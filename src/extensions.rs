ba121cfa7833a3498f1cec73027a83566b94025f
Igor Gapanovich <igapanovich@proton.me>
2026-02-04 20:17:06 +0100

    Fixed count_is_at_least for 0

--- src/extensions.rs
+++ src/extensions.rs
@@ -4,6 +4,10 @@ pub trait CountIsAtLeast {
 
 impl<T: Iterator> CountIsAtLeast for T {
     fn count_is_at_least(&mut self, n: usize) -> bool {
+        if n == 0 {
+            return true;
+        }
+
         let mut count = 0;
         for _ in self {
             count += 1;
